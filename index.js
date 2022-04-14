5d259a201dd7d4e2a60a0d67bd681dde39fccccb
MAALOUM-SAID <saaid_120@hotmail.com>
2022-04-14 12:20:11 +0000

    v3

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ let quiz={
             "a2":["échec ou succès",0],
             "a3":["un pointeur",0],
             "a4":["rien",1]
-        }
+        },
+        'discription':"Le constracteur retourne rien",
+    
     },
     question3 :{
         "Q":"Une interface peut-elle être instanciée ?",
@@ -15,7 +17,8 @@ let quiz={
             "a2":["non",1],
             "a3":["oui, toutes",0],
             "a4":["oui si elle est 'public'",0]
-        }
+        },
+        'discription':"non, l'interface ne pas instancier",
     },
     question4 :{
         "Q":"Une variable de classe, commune à toutes les instances d'une classe doit être déclarée ?",
@@ -24,7 +27,8 @@ let quiz={
             "a2":["public",0],
             "a3":["private",0],
             "a4":["global",0]
-        }
+        },
+        'discription':"satic , variable de class ",
     },
     question5 :{
         "Q":"Comment permettre à une méthode de gérer un nombre de paramètres variable en python ?",
@@ -33,7 +37,8 @@ let quiz={
             "a2":["utiliser des paramettres optionels",1],
             "a3":["la surcharger",0],
             "a4":["la déclarer en void",0]
-        }
+        },
+        'discription':"utiliser des paramettres optionels",
     },
     question6 :{
         "Q":"Dans quels langages une classe peut profiter de l'héritage multiple (hériter de plusieurs classes en même temps) ?",
@@ -42,7 +47,9 @@ let quiz={
             "a2":["C++",0],
             "a3":["Python",1],
             "a4":["PHP",0]
-        }
+        },
+        'discription':"Python",
+
     },
     question7 :{
         "Q":"Comment-appelle t-on le fait d'utiliser une 'classe' pour créer un 'objet'?",
@@ -51,14 +58,16 @@ let quiz={
             "a2":["construction",0],
             "a3":["surcharge",0],
             "a4":["implémentation",0]
-        }
+        },
+        'discription':"instanciation",
     }
 
 
 }
 
 // PAGE INDEX.HTML
-
+// 
+let result=document.getElementById('score');
 // card body
 let cardBody=document.getElementById('card-body');
 // inputs
@@ -71,9 +80,7 @@ let label1=document.getElementById('label1');
 let label2=document.getElementById('label2');
 let label3=document.getElementById('label3');
 let label4=document.getElementById('label4');
-// span
-let span=document.getElementById('span');
-let scorSpan=document.getElementById('score-span');
+
 // button
 let valider=document.getElementById('valider');
 let nextone=document.getElementById('nextone');
@@ -91,9 +98,12 @@ let initQuiz={
             "a2":["public ou protected",1],
             "a3":["toutes",0],
             "a4":["private ou public",0]
-        }
+        },
+        'discription':"public ou protected",
+        
     },
 }
+
 // labels
 header.textContent=initQuiz[`question${counter}`]['Q'];
 label1.textContent=initQuiz[`question${counter}`]["answers"]["a1"][0];
@@ -107,7 +117,8 @@ input3.value=initQuiz[`question${counter}`]["answers"]["a3"][1];
 input4.value=initQuiz[`question${counter}`]["answers"]["a4"][1];
 // add event listener
 let score=0;
-function next() {
+nextone.addEventListener('click',event=>{
+    if (event.target) {
         counter++;
         span.textContent=counter;
         if (counter<= size+1) {
@@ -128,7 +139,7 @@ function next() {
             nextone.remove();
             // link
             let link=document.createElement("a");
-            link.setAttribute("href",'result.html');
+            link.setAttribute("href","#score");
             let resultBtn=document.createElement("button");
             resultBtn.setAttribute('id','result');
             resultBtn.setAttribute("class","float-end btn btn-success");
@@ -141,24 +152,65 @@ function next() {
             let result_final=document.getElementById('result');
             result_final.addEventListener('click',event=>{
                 if (event.target) {
-                    valid();
-                    scorSpan.textContent=score;
-
+                    for (const form of document.forms) {
+                        for (const ele of form.elements) {
+                            if (ele.checked) {
+                                score+=parseInt(ele.value);
+                                document.getElementById('card-span').textContent=`
+                                    ${score} / ${size}
+                                `;
+                                if (score>=5) {
+                                    result.classList.add('border-success');
+                                }else{
+                                    result.classList.add('border-danger');
+                                }
+                                //
+                                let div=document.createElement('div'); 
+                                div.setAttribute('class','card');
+                                div.classList.add('m-3');
+                                div.classList.add('p-3');
+                                let h5=document.createElement("h5");
+                                h5.textContent=`Q1: ${initQuiz[`question1`]['Q']}`;
+                                let p=document.createElement("p");
+                                p.textContent=`${initQuiz[`question1`]['discription']}`;
+                                div.appendChild(h5);
+                                div.appendChild(p);
+                                document.body.appendChild(div)
+                                for (let i =2;i<=size+1;i++) {
+                                    let div=document.createElement('div'); 
+                                    div.setAttribute('class','card');
+                                    div.classList.add('m-3');
+                                    div.classList.add('p-3');
+                                    let h5=document.createElement("h5");
+                                    h5.textContent=`Q${i} : ${quiz[`question${i}`]['Q']}`;
+                                    let p=document.createElement("p");
+                                    p.textContent=`${quiz[`question${i}`]['discription']}`;
+                                    div.appendChild(h5);
+                                    div.appendChild(p);
+                                    document.body.appendChild(div);
+                                }
+                            }
+                        }
+                    }
+                    result.style.visibility='inherit';
+                    
                 }
             });
         }
         
-}
-function valid() {
-    for (const form of document.forms) {
-        for (const ele of form.elements) {
-            if (ele.checked) {
-                score+=parseInt(ele.value)
-                // span.textContent=score;
+    }
+
+});
+
+valider.addEventListener('click',event=>{
+    if (event.target) {
+        for (const form of document.forms) {
+            for (const ele of form.elements) {
+                if (ele.checked) {
+                    score+=parseInt(ele.value)  
+                }
             }
         }
     }
-}
-export {quiz , score};
-
+})
 
--- index2.js
+++
@@ -1,8 +0,0 @@
-import {quiz , score } from './index.js';
-
-let row=document.getElementById("row-res");
-let col=document.createElement('div');
-col.className="col";
-col.innerHTML="<h1>Hello</h1>";
-row.appendChild(col);
-// console.log(quiz);
